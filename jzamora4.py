40336e38b366e6074dd4b276a4f1a4411d178860
jzamora2433 <jzamora2433@gmail.com>
2026-08-20 01:37:40 +0000

    lab 4

--- jzamora4.py
+++ jzamora4.py
@@ -1,5 +1,34 @@
 #Josue Zamora
-#CTC 389 Lab 4
+# lab 4
 
-print("Josue Zamora")
+evens = [28,24,88,8,2, 2020,2022,2024, 188,30]
+
+print(evens[0])
+print(evens[1])
+print(evens[2])
+print(evens[3])
+print(evens[4])
+print(evens[5])
+print(evens[6])
+print(evens[7])
+print(evens[8])
+print(evens[9])
+
+print( "break")
+
+for i in evens:
+    print (i)
+
+frutable = ["mango", "pineapple", "watermelon", "zucchini", "eggplant"]
+
+print(frutable[0])
+print(frutable[1])
+print(frutable[2])
+print(frutable[3])
+print(frutable[4])
+
+print("break 2")
+
+for i in frutable:
+    print (i)
 
