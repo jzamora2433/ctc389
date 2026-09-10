a494400f47b2371439f221a3b3bb31e9b2325d4e
jzamora2433 <jzamora2433@gmail.com>
2026-09-10 00:58:03 +0000

    midtermquestion1

---
+++ midterm1.py
@@ -0,0 +1,8 @@
+#Josue Zamora
+#midterm question 1
+
+name = input("Plase give me your name: ")
+number = int(input("please give me a number: "))
+add = int(input("please tell me how much you would like to add to your original number: "))
+
+print(name, "you entered", number, "and added", add, "for a total of", number + add,)
