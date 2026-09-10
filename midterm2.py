a7fa75a2312cbb3709d07fdfe9fd1ee9d49405ea
jzamora2433 <jzamora2433@gmail.com>
2026-09-10 01:14:12 +0000

    question 2 midterm

---
+++ midterm2.py
@@ -0,0 +1,15 @@
+#Josue Zamora
+#midterm #2
+
+def area(b, h):
+    a = b*h
+    return a
+
+base = float(input("please enter the base of the rectangle: "))
+height = float(input("please give me the height of the rectangle: "))
+
+result = area(base, height)
+
+print("the area of your rectangle is", result)
+
+
